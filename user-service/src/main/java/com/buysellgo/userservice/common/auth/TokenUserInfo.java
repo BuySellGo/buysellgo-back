087e2aa831bef9cf54ee8b9624ce0ac45d6031f1
jh080724 <tsj010813@gmail.com>
2024-12-30 02:09:15 +0900

    project template commit

---
+++ user-service/src/main/java/com/buysellgo/userservice/common/auth/TokenUserInfo.java
@@ -0,0 +1,15 @@
+package com.buysellgo.userservice.common.auth;
+
+import com.buysellgo.userservice.common.entity.Role;
+import lombok.*;
+
+@Setter @Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenUserInfo {
+
+    private String email;
+    private Role role;
+
+}
