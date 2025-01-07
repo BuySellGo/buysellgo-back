b2b1ba50130961a885f463139988a81e3da40d39
minus43 <minu425854@gmail.com>
2025-01-07 09:55:47 +0900

    KAN-92 회원탈퇴(회원)

---
+++ user-service/src/main/java/com/buysellgo/userservice/common/util/CommonConstant.java
@@ -0,0 +1,25 @@
+package com.buysellgo.userservice.common.util;
+
+public enum CommonConstant {
+    // 메시지 상수
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다."),
+    TOKEN_OR_USER_NOT_FOUND("리프레시 토큰이 만료되었거나, 해당 사용자가 존재하지 않습니다."),
+    
+    // 토큰 상수
+    BEARER_PREFIX("Bearer "),
+    
+    // 응답 키값 상수
+    EMAIL("email"),
+    ROLE("role"),
+    FAILURE("failure");
+
+    private final String value;
+
+    CommonConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
