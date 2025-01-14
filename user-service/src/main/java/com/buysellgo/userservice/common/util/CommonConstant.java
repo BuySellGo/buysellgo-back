8e88ec90baeb04047ac94adee4da92a392f35dd9
minus43 <minu425854@gmail.com>
2025-01-14 15:28:14 +0900

    KAN-102 이메일 인증번호 발송(공통)-수정 완료

--- user-service/src/main/java/com/buysellgo/userservice/common/util/CommonConstant.java
+++ user-service/src/main/java/com/buysellgo/userservice/common/util/CommonConstant.java
@@ -38,7 +38,7 @@ public enum CommonConstant {
     USER_VO("userVo"),
     SELLER_VO("sellerVo"),
     ADMIN_VO("adminVo"),
-    SUCESS("success"),
+    SUCCESS("success"),
     FAILURE("failure"),
 
 
