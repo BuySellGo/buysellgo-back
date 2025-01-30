71cffc2b9fadd9d77c5e06e421ad1ad9ff3eb465
minus43 <minu425854@gmail.com>
2025-01-30 12:40:24 +0900

    KAN-17-Q&A-서비스-컨트롤러 생성

---
+++ qna-service/src/main/java/com/buysellgo/qnaservice/controller/QnaController.java
@@ -0,0 +1,46 @@
+package com.buysellgo.qnaservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.buysellgo.qnaservice.common.dto.CommonResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;   
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequestMapping("/qna")
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class QnaController {
+    
+
+    @Operation(summary ="Qna 작성(회원)")
+    @PostMapping("/question")
+    public ResponseEntity<CommonResDto<Map<String, Object>>> createQna(){
+        return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK, "Qna 작성 완료", null));  
+    }
+
+    @Operation(summary ="Qna 조회(회원,판매자,관리자)")
+    @GetMapping("/list")
+    public ResponseEntity<CommonResDto<Map<String, Object>>> getQna(){
+        return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK, "Qna 조회 완료", null));  
+    }
+
+    @Operation(summary ="Qna 삭제(회원,관리자)")
+    @DeleteMapping("/delete")   
+    public ResponseEntity<CommonResDto<Map<String, Object>>> deleteQna(){
+        return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK, "Qna 삭제 완료", null));  
+    }
+
+    @Operation(summary ="Qna 답변 작성(판매자)")
+    @PostMapping("/answer")
+    public ResponseEntity<CommonResDto<Map<String, Object>>> createAnswer(){
+        return ResponseEntity.ok().body(new CommonResDto<>(HttpStatus.OK, "Qna 답변 작성 완료", null));  
+    }
+}
--- qna-service/src/main/java/com/buysellgo/qnaservice/repository/QnaRepository.java
+++ qna-service/src/main/java/com/buysellgo/qnaservice/repository/QnaRepository.java
@@ -2,6 +2,7 @@ package com.buysellgo.qnaservice.repository;
 
 import com.buysellgo.qnaservice.entity.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface QnaRepository extends JpaRepository<Qna, Long> {
 }
