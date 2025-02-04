740a601192a3f247288e3622bdd1ebc2f9b6c427
minus43 <minu425854@gmail.com>
2025-02-05 06:22:54 +0900

    KAN-84,KAN-85,KAN-124,KAN-125,KAN-126-결제 수단 등록 조회 수정 삭제, 결제 생성 조회 상태변경 완료- 결제의 경우 판매자 아이디 추가 예정

--- payment-service/src/main/java/com/buysellgo/paymentservice/repository/PayMethodRepository.java
+++ payment-service/src/main/java/com/buysellgo/paymentservice/repository/PayMethodRepository.java
@@ -2,6 +2,10 @@ package com.buysellgo.paymentservice.repository;
 
 import com.buysellgo.paymentservice.entity.PayMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import java.util.Optional;
+    
 public interface PayMethodRepository extends JpaRepository<PayMethod, Long> {
+    List<PayMethod> findAllByUserId(long userId);
+    Optional<PayMethod> findByPayMethodName(String payMethodName);
 }
